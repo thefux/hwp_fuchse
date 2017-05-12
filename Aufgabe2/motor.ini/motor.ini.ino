0d635dd8b1b02e27fd3c2f637dd9f2471ec386b8
thefux <abdou.rakez@gmail.com>
2017-05-12 10:53:31 +0200

    motot

---
+++ Aufgabe2/motor.ini/motor.ini.ino
@@ -0,0 +1,36 @@
+#define PIN_10 10
+#define PIN_11 11
+
+bool forward = true;
+
+void setMotorSpeed(bool forward);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_10, OUTPUT);
+  pinMode(PIN_11, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  
+  setMotorSpeed(forward);
+  delay(2000);
+  
+  setMotorSpeed(!forward);
+  delay(2000);
+
+}
+
+void setMotorSpeed(bool forward) {
+  if (forward == true) {
+       digitalWrite(PIN_10, HIGH);
+       digitalWrite(PIN_11, LOW);
+  } else {
+      digitalWrite(PIN_11, HIGH);
+      digitalWrite(PIN_10, LOW);
+  }
+  
+}
+
