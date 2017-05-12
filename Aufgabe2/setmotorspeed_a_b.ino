5d63a3aca75fed133ae13be4dd4602af0431bff7
tobiasschaetzle <schaetzle_t@t-online.de>
2017-05-12 11:22:18 +0200

    Add files via upload
    bool motor in setMotorSpeed hinzugefügt

---
+++ Aufgabe2/setmotorspeed_a_b.ino
@@ -0,0 +1,57 @@
+#define A_Backwards 10
+#define A_Forward 11
+#define B_Forward  6
+#define B_Backwards  9
+
+
+
+bool forward = true;
+uint8_t spd = 50;
+bool motor = false;
+
+void setMotorSpeed(bool forward);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A_Forward, OUTPUT);
+  pinMode(A_Backwards, OUTPUT);
+  pinMode(B_Forward, OUTPUT);
+  pinMode(B_Backwards, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  
+  setMotorSpeed(forward, spd, motor);
+  delay(2000);
+
+
+}
+
+void setMotorSpeed(bool forward, uint8_t spd, bool motor){
+  if (motor){
+     if (forward == true) {
+       analogWrite(A_Forward, spd);
+       analogWrite(A_Backwards, 0);
+     } else {
+      analogWrite(A_Backwards, spd);
+      analogWrite(A_Forward, 0);
+     }
+  }else{
+    if (forward == true) {
+       analogWrite(B_Forward, spd);
+       analogWrite(B_Backwards, 0);
+     } else {
+      analogWrite(B_Backwards, spd);
+      analogWrite(B_Forward, 0);
+     }
+  
+  }
+}
+
+void driveForward(){
+  
+}
+
+
