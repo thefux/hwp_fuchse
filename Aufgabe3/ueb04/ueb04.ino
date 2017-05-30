81201ced5676610b900e1bd354335455b9345203
tobiasschaetzle <schaetzle_t@t-online.de>
2017-05-30 14:50:25 +0200

    Update ueb04.ino

--- Aufgabe3/ueb04/ueb04.ino
+++ Aufgabe3/ueb04/ueb04.ino
@@ -1,67 +1,19 @@
-void setup() {
-  Serial.begin(9600);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
 
+uint8_t counter = 0;
+uint8_t start = 0;
+uint8_t finish = 0;
+void setup() {
   
-  time_measure();
-  timer2();
+  pinMode(12, OUTPUT);
+  
 }
 
 void loop() {
     
-}
-void timer2() {
+  set_timer_freq(247);
   
-  // disable interrupts
-  cli();
-  
-  // reset control registers
-  TCCR2A = 0;     
-  TCCR2B = 0;    
-  
-  // set clock prescaler: 256
-  TCCR2B |= (1 << CS22);
-  TCCR2B |= (1 << CS20);
-  
-  // set mode (CTC)
-  TCCR2A |= (1 << WGM21);
-        
-  // set the output compare register (maximum value for now)
-  OCR2A = 255;
-  
-  // enable interrupt
-  TIMSK2 |= (1 << OCIE2A);
-    
-  sei();  
 }
 
-// Aufabe 4
-void timer2(void) {
-  
-  // disable all interrupts
-  cli();
-  
-  // reset control registers
-  TCCR2A = 0;     
-  TCCR2B = 0;    
-  
-  // set clock prescaler: 64
-  TCCR2B |= (1 << CS22);
-  // set mode (CTC)
-  TCCR2A |= (1 << WGM21);
-    
-  // set output compare register A
-  OCR2A = 255;
-    
-  // enable interrupt
-  TIMSK2 |= (1 << OCIE2A);    
-  
-  // eable all interrupts
-  sei();
-  
-}
-// Aufgabe 1
 void setPin12(boolean high){
   if (high) {
     PORTB |= (1 << 4);
@@ -71,54 +23,39 @@ void setPin12(boolean high){
   }
 }
 
-
-// Aufgabe 2
 void setPin12Asm(boolean high){
   if (high) {
     asm volatile (
-    "sbi %0, %1\n\t" // 2 clocks
+    "sbi %0, %1 \n" // 2 clocks
     :: "I" (_SFR_IO_ADDR(PORTB)), "I" (PORTB4));
   }
   else {
     asm volatile (
-    "cbi %0, %1\n\t" // 2 clocks
+    "cbi %0, %1 \n" // 2 clocks
     :: "I" (_SFR_IO_ADDR(PORTB)), "I" (PORTB4));
   }
  
 }
 
-// Aufgabe 3
-void time_measure(void) {
-  
-  
-  uint8_t tStart = millis();
-  uint8_t tPinFunction = 0;
-  
-  for (int i = 0; i != 10000; i++) {
-    setPin12(true);
-    setPin12(false);
-    // ownToggleAsm_off();
-  }
-  tPinFunction = millis() - tStart;
-  
-  uint8_t tAsmFunction = 0;
-  
-  for (int i = 0; i != 10000; i++) {
-    setPin12Asm(true);
-    setPin12Asm(false);
-  }
-  
-  Serial.println("setPin12 function");
-  Serial.print("measured time ");
-  Serial.println(tPinFunction);
-  
-  Serial.println("setPin12Asm function");
-  Serial.print("measured time ");
-  Serial.println(tAsmFunction);
-  
-  
+
+
+void set_timer_freq(int freq){
+    cli();
+    TCCR2A = 0;
+    TCCR2B = 0;
+    TCCR2B |= (1 << CS22);
+    TCCR2B |= (1 << CS20);
+    TCCR2A |= (1 << WGM21);
+    OCR2A = 625000/(freq*2);
+    TIMSK2 |= (1 << OCIE2A);
+    sei();
+     
 }
 
+ISR(TIMER2_COMPA_vect) {
+  // toggle pin
+  PINB ^= (1 << 4);
+}
 
 
 
